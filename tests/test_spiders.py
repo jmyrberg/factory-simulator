1055855305ebe6a5695edf3d2b1332783a0e19b0
Jesse Myrberg <jesse.myrberg@gmail.com>
2022-03-02 16:03:38 +0200

    Remove useless test

--- tests/test_spiders.py
+++ tests/test_spiders.py
@@ -1,12 +1,4 @@
-"""Module for testing spiders."""
+"""Module for testing spiders.
 
-
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-from crawler.spiders.listing_link import ListingSpider
-
-
-def test_listing_spider():
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(ListingSpider)
+TODO: Implement tests.
+"""
